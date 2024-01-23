8f0c6850fd93cdc17cae21ab5fdefa54f0001329
salimalisalim <salimalisalim@gmail.com>
2024-01-23 13:17:10 +0530

    changed vercel json 8

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-require("./bin/www");
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+
+dotenv.config({path:"./config.env"})
+
+app.get("/api/home", (req,res)=>{
+    res.send("Home route");
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log("Server is running on port 4000");
+})
